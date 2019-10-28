d609fd5643c4b2b15164eec02ce1f606c03245da
ruchir-123 <ruchir.mehta2112@gmail.com>
2019-10-28 23:45:43 +0530

    first commit

---
+++ precache-manifest.10e211c48ea64231149d495c5397dce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08c19c703d66b48c5fef339568b84fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dd6d6cbc388830a2dbf",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "787b6ce1c50ec73d059c",
+    "url": "/static/css/main.ab1b51a8.chunk.css"
+  },
+  {
+    "revision": "1dd6d6cbc388830a2dbf",
+    "url": "/static/js/2.80d5e3b7.chunk.js"
+  },
+  {
+    "revision": "787b6ce1c50ec73d059c",
+    "url": "/static/js/main.5e92c4aa.chunk.js"
+  },
+  {
+    "revision": "e1b254299f4ca47a4251",
+    "url": "/static/js/runtime-main.f09db7f8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
