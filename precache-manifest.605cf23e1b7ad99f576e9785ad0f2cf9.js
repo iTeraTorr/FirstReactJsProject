bec524a052a03df416cebf542991cf5b47998491
ruchir-123 <ruchir.mehta2112@gmail.com>
2019-10-29 00:14:27 +0530

    Updates

---
+++ precache-manifest.605cf23e1b7ad99f576e9785ad0f2cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a74550e2ce27517acc209f39ba7f528",
+    "url": "/FirstReactJsProject/index.html"
+  },
+  {
+    "revision": "1dd6d6cbc388830a2dbf",
+    "url": "/FirstReactJsProject/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bb70bf508aca8c3658b5",
+    "url": "/FirstReactJsProject/static/css/main.ab1b51a8.chunk.css"
+  },
+  {
+    "revision": "1dd6d6cbc388830a2dbf",
+    "url": "/FirstReactJsProject/static/js/2.80d5e3b7.chunk.js"
+  },
+  {
+    "revision": "bb70bf508aca8c3658b5",
+    "url": "/FirstReactJsProject/static/js/main.29065f97.chunk.js"
+  },
+  {
+    "revision": "e0a5229011bc544a386e",
+    "url": "/FirstReactJsProject/static/js/runtime-main.fbfa1670.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/FirstReactJsProject/static/media/logo.25bf045c.svg"
+  }
+]);
